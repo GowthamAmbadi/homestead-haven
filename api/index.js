ec15a42ad95bfa76328959ea731d71f2ebf9baf6
Gowtham <gowthamkumar44544@gmail.com>
2024-04-28 19:56:09 +0530

    update header , private profile

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ import authRouter from './routes/auth.route.js'
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
-  console.log('Connected to MongoDB!');
+  console.log('Connected to MongoDB');
 }).catch((err)=>{
   console.log(err);
 })
